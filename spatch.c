68af255673e7c77e00f392221ce6d3da67a1d08e
root <root@debian>
2017-02-16 19:25:27 +0100

    v2

--- spatch.c
+++ spatch.c
@@ -1,7 +1,9 @@
 #include <libssh/libssh.h>
 #include <libssh/server.h>
 #include <libssh/callbacks.h>
-
+//#include <libssh/legacy.h>
+#include <libssh/sftp.h>
+#include <libssh/ssh2.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -15,50 +17,76 @@
 #include <fcntl.h>
 #include <time.h>
 
+int ssh_handle_key_exchange(ssh_session session);
+
+
 int main(int argc, char **argv)
 {
-	unsigned int port = 22;
-	const char *hostname = "192.168.1.58";
+  unsigned int port = 50555;
+	const char *hostname = "192.168.0.14";
 	const char *RSA_key = "/etc/ssh/ssh_host_rsa_key";
+	const char *DSA_key = "/etc/ssh/ssh_host_dsa_key";
 	int log = SSH_LOG_PROTOCOL;
 	ssh_bind sshbind;
 	ssh_session session;
 	int sok;
 	struct sockaddr_in sin;
+	struct sockaddr_in bind_stock;
 	unsigned long hostaddr;
 	struct protoent *protocol;
+	int ret;
 	
 	protocol = getprotobyname("TCP");
 
-	if ((sok = socket(AF_INET, SOCK_STREAM, protocol->p_proto)) == -1) {
-	  dprintf(2, "error open socket\n");
-	}
-	else {
-	  dprintf(1, "Socket opened\n");
-	}
-
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(22);
-	sin.sin_addr.s_addr = INADDR_ANY;
+		if ((sok = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	  dprintf(2, "error open socket\n"); 
+	else
+	  dprintf(1, "socket opened\n");
 	
+
+	
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = inet_addr("192.168.0.14"); 
+	/*
+	if (bind(sok, (const struct sockaddr *) &sin, sizeof(sin)) ==-1)
+	 perror("bind");
+	
+	if (listen(sok, SOMAXCONN) == -1)
+	 perror("listen");
+	*/
 	ssh_init();
-	session = ssh_new();
+	
 	sshbind = ssh_bind_new();
+	session = ssh_new();
+	
 	ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDPORT_STR,  &port);
 	ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDADDR, hostname);
 	ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_LOG_VERBOSITY, &log);
 	ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_LOG_VERBOSITY_STR, &log);
 	ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_HOSTKEY, RSA_key);
+	ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_HOSTKEY, DSA_key);
 	ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_RSAKEY, RSA_key);
+	ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_DSAKEY, DSA_key);
+	
 	if (ssh_bind_listen(sshbind) < 0)
 	  fprintf(stderr,"error bind %s\n", ssh_get_error(sshbind));
 	ssh_bind_set_blocking(sshbind, 0);
-	while (1) {
-	  //dprintf(1,"WAITING\n", strlen("WAITING\n"));
-	  if (ssh_bind_accept(sshbind, session) == SSH_ERROR)
-	    fprintf(stderr,"Error accept %s\n", ssh_get_error(sshbind));
-	  else
-	    dprintf(1,"Accepted", strlen("Accepted"));
+
+	if (ssh_bind_accept(sshbind, session) == SSH_ERROR)
+	     fprintf(stderr,"Error accept %s\n", ssh_get_error(sshbind));
+	else
+	      dprintf(1,"connection Accepted", strlen("connection Accepted"));
+	
+	ssh_handle_key_exchange(session); 
+	  fprintf(stderr, "ssh_handle_key_exchange: %s\n", ssh_get_error(session));
+
+	  ssh_set_auth_methods(session, SSH_AUTH_METHOD_PASSWORD);
+
+	  
+	  while (1) {
+	  dprintf(1,"WAITING\n", strlen("WAITING\n"));
+	
 	}
 	ssh_bind_free(sshbind);
 	ssh_free(session);
